9c3dd0ba3464f8e64a43403e154dae4379bddf05
Julian <julian.koehn1989@googlemail.com>
2023-01-07 20:15:01 +0100

    add switch function to button

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// const button = document.getElementById('switch');
+// const recess = document.getElementById('recess');
+
+// console.log(button);
+// console.log(recess);
+
+// button.addEventListener('click', () => {
+//   recess.classList.toggle('active');
+// });
+
+const button = document.querySelector('.switch');
+const recess = document.querySelector('.recess');
+
+console.log(button);
+console.log(recess);
+
+button.addEventListener('click', () => {
+  recess.classList.toggle('active');
+});
