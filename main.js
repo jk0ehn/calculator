016fe5c569586bb19de25609cb291e4bf46bed4d
Julian <julian.koehn1989@googlemail.com>
2023-01-08 20:03:47 +0100

    display number

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 
 const button = document.querySelector('.switch');
 const recess = document.querySelector('.recess');
+const numbers = document.querySelector('.displayed-numbers');
+
+console.log(numbers);
 
 button.addEventListener('click', () => {
   recess.classList.toggle('active');
