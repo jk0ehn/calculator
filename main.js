cf6bbd10cb029df0dad2a227e835b275f6194821
Julian <julian.koehn1989@googlemail.com>
2023-01-08 17:16:46 +0100

    display numbers

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 'use strict';
 
-// const button = document.getElementById('switch');
-// const recess = document.getElementById('recess');
-
-// console.log(button);
-// console.log(recess);
-
-// button.addEventListener('click', () => {
-//   recess.classList.toggle('active');
-// });
-
 const button = document.querySelector('.switch');
 const recess = document.querySelector('.recess');
-
-console.log(button);
-console.log(recess);
 
 button.addEventListener('click', () => {
   recess.classList.toggle('active');
